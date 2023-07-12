f5ede5e2e25e1c6f42d7b6a4b699c79a006b8071
Andrew Robertson <andy.d.robertson@gmail.com>
2023-07-12 11:51:47 +0100

    import bug

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='streamlithelpers',
     packages=['streamlithelpers'],
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Simple utilities to help streamlit app development',
     author='Andrew Robertson',
     author_email='',
     url='https://github.com/andehr/streamlit-helpers',
-    download_url='https://github.com/andehr/streamlit-helpers/archive/refs/tags/v0.1.0.tar.gz',
+    download_url='https://github.com/andehr/streamlit-helpers/archive/refs/tags/v0.1.1.tar.gz',
     keywords=['streamlit', 'utility'],
     install_requires=[
         'pandas',
