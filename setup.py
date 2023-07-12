9f5c5945b028dafa61f699bf2e55add592af60da
Andrew Robertson <andy.d.robertson@gmail.com>
2023-07-12 11:42:52 +0100

    download url

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='streamlithelpers',
     packages=['streamlithelpers'],
-    version='0.1',
+    version='0.1.0',
     license='MIT',
     description='Simple utilities to help streamlit app development',
     author='Andrew Robertson',
     author_email='',
     url='https://github.com/andehr/streamlit-helpers',
-    download_url='https://github.com/andehr/streamlit-helpers/archive',
+    download_url='https://github.com/andehr/streamlit-helpers/archive/refs/tags/v0.1.0.tar.gz',
     keywords=['streamlit', 'utility'],
     install_requires=[
         'pandas',
